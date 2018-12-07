88a84d42dc60223e0a748faac338d9bb84e79a2b
Christos Dousmanopoulos <system-error@users.noreply.github.com>
2018-12-07 20:37:13 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.IO;
 using System.Collections.Generic;
 
@@ -16,6 +16,9 @@ namespace ConsoleApp1
                 string[] _values = null;
                 int x = 0;
                 var sum = 0;
+                var counter = 0;
+                float total = 0;
+                float average = 0;
                 //List<int> listOfProductsSold = new List<int>();
                 List<takeTheColums> listOfColumns = new List<takeTheColums>();
                 //Dictionary<string, int> listOfAges = new Dictionary<string, int>();
@@ -40,12 +43,38 @@ namespace ConsoleApp1
                     //Console.WriteLine(listOfColumns[y].productsSold);
                     sum += listOfColumns[y].productsSold;
                 }
+                Console.WriteLine("The total products are: " + sum);
+                for (int z = 0; z<listOfColumns.Count-1; z++)
+                {
+                    var previousAge = listOfColumns[z].ages;
+                    float theFirtsElement = listOfColumns[z].purchases;
+                    total = theFirtsElement;
+                    counter = 1;
+                    for (int j = z + 1; j < listOfColumns.Count;)
+                    {
+                        var nextAge = listOfColumns[j].ages;
+                        if (previousAge.Equals(nextAge))
+                        {
+                            
+                            counter += 1;
+                            total += listOfColumns[j].purchases;
+                            Console.WriteLine("The previous age is {0} and the next age is {1}", previousAge, nextAge);
+                            listOfColumns.RemoveAt(j);
+
+
+                        }
+                        else { j++; }
+                    }
+                    average = total / counter;
+                }
+                
+                
                 /*foreach (int z in listOfColumns[].productsSold)
                 {
                     sum += z;
                 }*/
-                
-                Console.WriteLine("The total products are: " + sum);
+
+
 
                 //for (int y=0; y<listOfProductsSold.count; y++)
                 //{
@@ -63,7 +92,10 @@ namespace ConsoleApp1
             public int purchases;
             public int productsSold;
             }
+
         
         
     }
 }
+
+//string filePath = @"path_of_the_file\BlackFriday1.csv";
