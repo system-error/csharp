d1c890c48d69a8673fd5d9cf6183ac572b6ebdfa
Christos Dousmanopoulos <system-error@users.noreply.github.com>
2018-12-08 11:51:32 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,92 +8,81 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            string filePath = @"path_of_the_file\BlackFriday.csv";
+            string strline = "";
+            string[] _values = null;
+            int x = 0;
+            var sum = 0;
+            var counter = 0;
+            float total = 0;
+            float average = 0;
+            List<takeTheColums> listOfColumns = new List<takeTheColums>();
+            string filePath = @"path_of_the_file\BlackFriday1.csv";
+
             using (StreamReader sr = new StreamReader(filePath))
             {
-                string strline = "";
-                string[] _values = null;
-                int x = 0;
-                var sum = 0;
-                var counter = 0;
-                float total = 0;
-                float average = 0;
-                //List<int> listOfProductsSold = new List<int>();
-                List<takeTheColums> listOfColumns = new List<takeTheColums>();
-                //Dictionary<string, int> listOfAges = new Dictionary<string, int>();
                 while (!sr.EndOfStream)
                 {
                     x++;
                     strline = sr.ReadLine();
                     _values = strline.Split(',');
-                    
+
                     if (x >= 2)
                     {
-                        //listOfProductsSold.Add();
-                        listOfColumns.Add(new takeTheColums { ages = _values[3], productsSold = int.Parse(_values[8]), purchases = int.Parse(_values[11]) });     
+                        listOfColumns.Add(new takeTheColums { ages = _values[3], productsSold = int.Parse(_values[8]), purchases = int.Parse(_values[11]) });
                     }
                 }
-                
-                //string gaes = listOfAges[0].ages;
 
-                //Console.WriteLine(gaes);
-
-                for (int y = 0; y<listOfColumns.Count; y++)
-                {
-                    //Console.WriteLine(listOfColumns[y].productsSold);
-                    sum += listOfColumns[y].productsSold;
-                }
+                sum = findTheSum(sum, listOfColumns);
                 Console.WriteLine("The total products are: " + sum);
-                for (int z = 0; z<listOfColumns.Count-1; z++)
-                {
-                    var previousAge = listOfColumns[z].ages;
-                    float theFirtsElement = listOfColumns[z].purchases;
-                    total = theFirtsElement;
-                    counter = 1;
-                    for (int j = z + 1; j < listOfColumns.Count;)
-                    {
-                        var nextAge = listOfColumns[j].ages;
-                        if (previousAge.Equals(nextAge))
-                        {  
-                            counter += 1;
-                            total += listOfColumns[j].purchases;
-                            //Console.WriteLine("The previous age is {0} and the next age is {1}", previousAge, nextAge);
-                            listOfColumns.RemoveAt(j);
-                        }
-                        else { j++; }
-                    }
-                    average = total / counter;
-                    Console.WriteLine("The average of ages between {0} is: {1}", previousAge,average);
-                }
+
+                findTheAverage(ref counter, ref total, ref average, listOfColumns);
                 Console.WriteLine("Finished!");
-
-
-                /*foreach (int z in listOfColumns[].productsSold)
-                {
-                    sum += z;
-                }*/
-
-               //for (int y=0; y<listOfProductsSold.count; y++)
-               //{
-               //    sum += listOfProductsSold[y];                  works with for loop too  
-               //}
-               //console.writeline("the total products are: " + sum);
-
-               Console.ReadKey();
+                Console.ReadKey();
                 sr.Close();
             }
-            //Console.WriteLine(sum);*/
         }
+
+        private static void findTheAverage(ref int counter, ref float total, ref float average, List<takeTheColums> listOfColumns)
+        {
+            for (int z = 0; z < listOfColumns.Count - 1; z++)
+            {
+                var previousAge = listOfColumns[z].ages;
+                float theFirtsElement = listOfColumns[z].purchases;
+                total = theFirtsElement;
+                counter = 1;
+                for (int j = z + 1; j < listOfColumns.Count;)
+                {
+                    var nextAge = listOfColumns[j].ages;
+                    if (previousAge.Equals(nextAge))
+                    {
+                        counter += 1;
+                        total += listOfColumns[j].purchases;
+                        listOfColumns.RemoveAt(j);
+                    }
+                    else { j++; }
+                }
+                average = total / counter;
+                Console.WriteLine("The average of ages between {0} is: {1}", previousAge, average);
+            }
+        }
+
+        private static int findTheSum(int sum, List<takeTheColums> listOfColumns)
+        {
+            for (int y = 0; y < listOfColumns.Count; y++)
+            {
+                sum += listOfColumns[y].productsSold;
+            }
+
+            return sum;
+        }
+
         class takeTheColums{
             public string ages;
             public int purchases;
             public int productsSold;
-            }
-
-        
-        
+            } 
     }
 }
+
 
 //string filePath = @"path_of_the_file\BlackFriday1.csv";
